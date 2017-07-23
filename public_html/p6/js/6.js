fa1283810ee4406394efa036001d8a01f5f204fc
Kleur <Kleur@Kleur>
2017-07-23 17:48:49 -0500

    Punto 6 terminado, me confundí un poco al inicio pues no sabía como iniciar las operaciones matemáticas.

---
+++ public_html/p6/js/6.js
@@ -0,0 +1,23 @@
+var result = Math.pow(50 , 10);
+   console.log("50 ^ 10 = " + result);
+   
+var result = Math.pow(10 , 50);
+   console.log("10 ^ 50 = " + result);
+   
+var result = 50*10;
+   console.log("50*10 =" + result);
+   
+var result = 50/10;
+   console.log("50/10 =" + result);
+   
+var result = 10/50;
+   console.log("10/50 =" + result);
+   
+var result = 50%10;
+   console.log("50%10 =" + result);
+   
+var result = 50-10;
+   console.log("50-10 =" + result);
+   
+var result = 50+10;
+   console.log("50+10 =" + result);
