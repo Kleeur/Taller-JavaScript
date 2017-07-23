3ed9b62cf4d75d79f2a7512897b0e5f36f15f081
Kleur <Kleur@Kleur>
2017-07-23 17:28:31 -0500

    Punto 5 terminado, sencillo.

---
+++ public_html/p5/js/5.js
@@ -0,0 +1,6 @@
+var name = prompt ("Type here your full name");
+var idtype = prompt ("Type here your id type");
+var id = prompt ("Type here your id");
+
+console.log("Hello, my name is " + name + ".");
+console.log("My number of " + idtype + " is " + id + ".");
