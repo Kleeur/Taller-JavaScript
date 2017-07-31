6e9f0d22f86fc178b2715f41a9c953825717eed0
Kleur <Kleur@Kleur>
2017-07-31 16:18:38 -0500

    Punto 11 terminado, tuvieron que explicarme

---
+++ public_html/P11/js/11.js
@@ -0,0 +1,10 @@
+var pussay1 = "";
+var pussay = "*"; i = 0;
+do {
+   pussay1 = pussay1 + pussay;
+   i++;
+   console.log(pussay1);
+   if (pussay1 === "******") {
+var i = 6;
+}
+} while (i <= 5);
