8d5ccb64e408a4150783032ae9d0c8ae9a5824ca
Kleur <Kleur@Kleur>
2017-07-31 16:50:01 -0500

    Punto 16 terminado, me explicaron-

---
+++ public_html/P16/js/16.js
@@ -0,0 +1,8 @@
+var num1 = parseInt(prompt('Type the base'));
+var num2 = parseInt(prompt('Type the height'));
+
+perimeter = (num1+num1) + (num2+num2);
+   console.log("Perimeter is  = " + perimeter);
+   
+area = (num1*num2);
+   console.log("Area is = " + area);
