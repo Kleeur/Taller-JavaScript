61a05e5aa62472d04feff2c7df97a95ff13c35f4
Kleur <Kleur@Kleur>
2017-08-02 13:03:13 -0500

    Punto 12 listo, demasiado raro

---
+++ public_html/P12/js/12.js
@@ -0,0 +1,10 @@
+var pussay = "     *";
+var pussay1 = "*";
+var pussay2 = 1;
+while(pussay2 <= 5) {
+   if (pussay === "     *") {
+      console.log(pussay);
+   }
+   console.log(pussay = (pussay.slice(1) + pussay1));
+   pussay2++;
+}
