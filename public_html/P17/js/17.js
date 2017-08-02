0ea90d914ea7cb9b8c214a5eeeab7da678795623
Kleur <Kleur@Kleur>
2017-08-02 13:01:09 -0500

    Punto 17 listo, sencillo.

---
+++ public_html/P17/js/17.js
@@ -0,0 +1,6 @@
+ var n1 = parseInt(prompt("Type one number"));
+ var n2 = parseInt(prompt("Type a second number"));
+ var n3 = parseInt(prompt("Type a third number"));
+ 
+ var x = n1*n2/n3;
+ console.log("x = " + n1 + " * "+ n2 + " / " + n3 + " = " + x);
