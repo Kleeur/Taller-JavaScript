cda1f42e12810a735968f40caed0fe828c485c7e
Kleur <Kleur@Kleur>
2017-09-05 13:57:48 -0500

    Punto 22 terminado

---
+++ public_html/P22/js/22.js
@@ -0,0 +1,15 @@
+var years = 15;
+var yearm = 12;
+var yeard = 365;
+var months = 15 * 12;
+var days = 12 * 365;
+var hours = (24 * 365) * 15;
+var minutes = (((60 * 24) * 365) * 15);
+var seconds = parseInt(minutes * 60);
+
+console.log("15 Years contain:");
+console.log(months + " Months");
+console.log(days + " Days");
+console.log(hours + " Hours");
+console.log(minutes + " Minutes");
+console.log(seconds + " Seconds");
