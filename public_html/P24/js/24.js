f01b08c42b317954cf816b5eaf438c0a3b928487
Kleur <Kleur@Kleur>
2017-09-05 16:11:49 -0500

    Punto 24 terminado

---
+++ public_html/P24/js/24.js
@@ -0,0 +1,6 @@
+var hours = parseInt(prompt("Type a number of hours"));
+var days = Math.floor(hours / 24);
+var weeks = Math.floor(days / 7);
+
+console.log("Total days: " + days);
+console.log("Total weeks: " + weeks); 
