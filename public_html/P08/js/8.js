80b079bb4543023c5ed616a3097f1e9f0cc45871
Kleur <Kleur@Kleur>
2017-09-13 14:09:55 -0500

    terminado

---
+++ public_html/P08/js/8.js
@@ -0,0 +1,14 @@
+var number1 = prompt("Type one number");
+var number2 = prompt("Type another number");
+
+result = parseInt(number1)+parseInt(number2);
+   console.log(number1 + " + "+ number2 + " = " + result);
+   
+result = parseInt(number1)-parseInt(number2);
+   console.log(number1 + " - "+ number2 + " = " + result);
+   
+result = parseInt(number1)*parseInt(number2);
+   console.log(number1 + " * "+ number2 + " = " + result);
+   
+result = parseInt(number1)/parseInt(number2);
+   console.log(number1 + " / "+ number2 + " = " + result);
